5f78f9138d7e2afd6c592d0e9c60540259660499
jeff@mobilefringe.com @ CodeCloud <jeff@mobilefringe.com @ CodeCloud>
2019-03-21 15:01:49 -0400

    Creekside Town Center 2.0 1.0  /map.vue

--- map.vue
+++ map.vue
@@ -75,7 +75,7 @@
             },
             created (){
                 this.loadData().then(response => {
-                    var temp_repo = this.findRepoByName('Directory Banner');
+                    var temp_repo = this.findRepoByName('Map Banner');
                     if(temp_repo !== null && temp_repo !== undefined) {
                        temp_repo = temp_repo.images;
                        this.pageBanner = temp_repo[0];
