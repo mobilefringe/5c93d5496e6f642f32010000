26baed0ae3eae46b9fa8523a3e8d082c97362f54
jeff@mobilefringe.com @ CodeCloud <jeff@mobilefringe.com @ CodeCloud>
2019-03-21 15:54:49 -0400

    Creekside Town Center 2.0 1.0  /jobs.vue

--- jobs.vue
+++ jobs.vue
@@ -19,7 +19,7 @@
 			},
 			created() {
 				this.$store.dispatch("getData", "events").then(response => {
-				    var temp_repo = this.findRepoByName('Events Banner').images;
+				    var temp_repo = this.findRepoByName('Jobs Banner').images;
                     if(temp_repo != null) {
                         this.pageBanner = temp_repo[0];
                     } else {
