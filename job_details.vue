b5cb868c959cd0e0ab34402b06c5b4824202a85e
jeff@mobilefringe.com @ CodeCloud <jeff@mobilefringe.com @ CodeCloud>
2019-03-22 14:37:11 -0400

    Creekside Town Center 2.0 1.0  /job_details.vue

--- job_details.vue
+++ job_details.vue
@@ -1,6 +1,5 @@
 <template>
     <div> <!-- Without an outer container div this component template will not render -->
-        <p>hello</p>
         <loading-spinner v-if="!dataLoaded"></loading-spinner>
         <transition name="fade">
             <div v-if="dataLoaded" v-cloak>
