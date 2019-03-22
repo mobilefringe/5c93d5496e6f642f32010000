76b9d4d0e3eb56db6128d8ae4b38af44d3f42de7
jeff@mobilefringe.com @ CodeCloud <jeff@mobilefringe.com @ CodeCloud>
2019-03-22 13:30:23 -0400

    Creekside Town Center 2.0 1.0  /jobs.vue

--- jobs.vue
+++ jobs.vue
@@ -21,7 +21,7 @@
                                     <h4 class="event_name">name</h4>
                                     <p class="event_dates">date</p>
                                     <div class="event_desc">name</div>
-                                    <router-link :to="{ name: 'eventDetails', params: { id: event.slug, banner: pageBanner }}">
+                                    <router-link :to="{ name: 'eventDetails', params: { id: name, banner: pageBanner }}">
                                         <div class="animated_btn event_link">View Event Details <i class="fas fa-angle-double-right"></i></div>
                                     </router-link>
                                     <hr class="event_seperator">
