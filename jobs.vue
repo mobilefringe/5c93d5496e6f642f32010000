a42214efdd1d419b03613811d96aef613c86ce16
jeff@mobilefringe.com @ CodeCloud <jeff@mobilefringe.com @ CodeCloud>
2019-03-22 13:11:39 -0400

    Creekside Town Center 2.0 1.0  /jobs.vue

--- jobs.vue
+++ jobs.vue
@@ -127,6 +127,7 @@
                         }
                     });
                     temp_promo = _.sortBy(temp_promo, ['created_at', 'start_date']).reverse();
+                    console.log(temp_promo)
                     return temp_promo;
                 },
                 promoList: function promos() {
