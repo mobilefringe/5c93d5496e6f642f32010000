26995229715aa3088d5787087f5cb324463f0efb
jeff@mobilefringe.com @ CodeCloud <jeff@mobilefringe.com @ CodeCloud>
2019-03-22 14:43:00 -0400

    Creekside Town Center 2.0 1.0  /job_details.vue

--- job_details.vue
+++ job_details.vue
@@ -19,9 +19,7 @@
                             <div class="col-md-8">
                                 <h4 class="event_name">{{ currentEvent.name }}</h4>
                                 <p class="event_type">{{currentEvent.job_type}}</p>
-                                <p class="event_details_dates">
-                                    <span>{{ currentEvent.start_date | moment("MMMM D", timezone)}}</span>
-                                </p>
+                                <p class="event_dates">{{events.start_date | moment("MMM D", timezone)}} - {{events.end_date | moment("MMM D", timezone)}}</p>
                                 <div class="event_desc event_details" v-html="currentEvent.rich_description"></div>
                             </div>
                         </div>
