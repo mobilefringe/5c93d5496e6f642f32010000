0f2c1be59f6fb4a17dee9db3702eb4ae13e3154d
jeff@mobilefringe.com @ CodeCloud <jeff@mobilefringe.com @ CodeCloud>
2019-03-26 16:23:24 -0400

    Creekside Town Center 2.0 1.0  /header.vue

--- header.vue
+++ header.vue
@@ -26,7 +26,7 @@
     						        <span @click="showDropDown = !showDropDown" v-if="item.sub_menu != undefined">{{ item.name }}</span>
     						        <ul v-show="showDropDown" v-if="item.sub_menu">
     						            <li v-for="sub_menu in item.sub_menu" class="dropdown_item">
-    						                <router-link @click="showDropDown = !showDropDown :to="sub_menu.href">{{ sub_menu.name }}</router-link>
+    						                <router-link @click="showDropDown = !showDropDown" :to="sub_menu.href">{{ sub_menu.name }}</router-link>
     						            </li>
     								</ul>
     						    </li>
