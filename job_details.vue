8a086472a5674ae2508de45a115423071f8f2b6c
jeff@mobilefringe.com @ CodeCloud <jeff@mobilefringe.com @ CodeCloud>
2019-03-26 14:26:27 -0400

    Creekside Town Center 2.0 1.0  /job_details.vue

--- job_details.vue
+++ job_details.vue
@@ -76,7 +76,7 @@
                         this.pageBanner = temp_repo[0];
                     } else {
                         this.pageBanner = {
-                            "image_url": "//codecloud.cdn.speedyrails.net/sites/5b5f2c136e6f644fcb5b0100/image/jpeg/1529532304000/insidebanner2.jpg"
+                            "image_url": "//codecloud.cdn.speedyrails.net/sites/5c93d5496e6f642f32010000/image/png/1553624485505/creekside_banner.png"
                         }
                     }
                     
