c24c5763db917739a78c43904fea99405726d984
alex@mobilefringe.com @ CodeCloud <alex@mobilefringe.com @ CodeCloud>
2019-04-02 10:32:51 -0400

    Creekside Town Center 2.0 1.0  /thank_you.vue

--- thank_you.vue
+++ thank_you.vue
@@ -58,7 +58,7 @@
                         }
                     }
                     if(response && response[1]){
-                        this.main = response[1].data;
+                        this.thankYou = response[1].data;
                         if(response[1].data && response[1].data.subpages && response[1].data.subpages[0]){
                             this.leasingInfo = response[1].data.subpages[0]
                         }
