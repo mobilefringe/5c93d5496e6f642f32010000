89ce7fcd5a10699bbeb4c0110e34b2f87c9e4c4b
alex@mobilefringe.com @ CodeCloud <alex@mobilefringe.com @ CodeCloud>
2019-04-02 10:29:56 -0400

    Creekside Town Center 2.0 1.0  /thank_you.vue

--- thank_you.vue
+++ thank_you.vue
@@ -16,7 +16,7 @@
                     </div>
                     <div class="row">
                         <div class="col-md-12">
-                            <div class="leasing_main" v-if="main" v-html="main.body"></div>
+                            <div v-if="thankYou" v-html="main.body"></div>
                         </div>
                     </div>
                     </div>
