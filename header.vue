8027ed7912b98264f4c007527279a1b75d7ea8ad
alex@mobilefringe.com @ CodeCloud <alex@mobilefringe.com @ CodeCloud>
2019-04-04 10:48:28 -0400

    Creekside Town Center 2.0 1.0  /header.vue

--- header.vue
+++ header.vue
@@ -21,11 +21,11 @@
                     <div class="col-md-12">
                         <nav id="primary_nav">
     						<ul>
-    						    <li class="menu_item" v-for="item in menu_items" :id="item.id">
+    						    <li class="menu_item" v-for="item in menu_items" :id="item.id" @mouseleave="showDropDown = false" @mouseover="showDropDown = true">
     						        <router-link v-if="item.sub_menu == undefined" :to="item.href">{{ item.name }}</router-link>
     						        <span @click="showDropDown = !showDropDown" v-if="item.sub_menu != undefined">{{ item.name }}</span>
-    						        <ul v-if="item.sub_menu">
-    						            <li v-for="sub_menu in item.sub_menu" class="dropdown_item">
+    						        <ul v-show="showDropDown" v-if="item.sub_menu">
+    						            <li @click="showDropDown = !showDropDown" v-for="sub_menu in item.sub_menu" class="dropdown_item">
     						                <router-link :to="sub_menu.href">{{ sub_menu.name }}</router-link>
     						            </li>
     								</ul>
