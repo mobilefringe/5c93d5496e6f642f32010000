f8d9ba9acdd7435daf0fd468d0cf6455fbe9f505
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2019-05-31 10:41:53 -0400

    Creekside Town Center 2.0 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -203,6 +203,7 @@
                             }
                         }
                     });
+                    console.log("this.$store.state.banners", this.$store.state.banners)
                     // var temp_image_url = ["//codecloud.cdn.speedyrails.net/sites/5c93d5496e6f642f32010000/image/jpeg/1557960136000/CTC_HungryTreat319_images1.jpg", "//codecloud.cdn.speedyrails.net/sites/5c82cb8f6e6f643f0f010000/image/jpeg/1553012778499/hungry_1920x450_1.jpg"]
                     // var temp_name = ["Treat Yourself.","HUNGRY? "]
                     // var temp_desc = ["You deserve it.", "We know just the place..."]
