c182e692932d37c6f47f36ebc997cbd0186a4cd3
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-11-14 11:42:38 -0500

    Creekside Town Center 2.0 1.2  /events_and_promotions.vue

--- events_and_promotions.vue
+++ events_and_promotions.vue
@@ -62,7 +62,7 @@
                                                 {{ item.store.name }}
                                             </router-link>        
                                         </p>
-                                        <h4 class="event_name">{{ item.name }}</h4>
+                                        <h3 class="event_name">{{ item.name }}</h3>
                                         <p class="event_dates"><span v-if="isMultiDay(item)">{{ item.start_date | moment("MMMM D", timezone)}} - {{ item.end_date | moment("MMMM D", timezone)}}</span><span v-else>{{ item.start_date | moment("MMMM D", timezone)}}</span></p>
                                         <div class="event_desc" v-html="item.description_short"></div>
                                         <router-link :to="{ name: 'promotionDetails', params: { id: item.slug, banner: pageBanner }}">
