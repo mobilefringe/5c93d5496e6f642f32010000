2c0046ad28562b95870ac678500c0376b9813750
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-11-14 12:53:04 -0500

    Creekside Town Center 2.0 1.2  /home.vue

--- home.vue
+++ home.vue
@@ -190,7 +190,8 @@
                     'timezone',
                     'getPropertyHours',
                     'processedPromos',
-                    'processedEvents'
+                    'processedEvents',
+                    'findMetaDataByPath'
                 ]),
                 homeBanners() {
                     var banners = [];
