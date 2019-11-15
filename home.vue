b565d886cf502ac4bd3a4d89bbde856fe0f9429b
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-11-14 19:31:16 -0500

    Creekside Town Center 2.0 1.2  /home.vue

--- home.vue
+++ home.vue
@@ -25,7 +25,7 @@
                                 <div class="banner_image" v-bind:style="{ backgroundImage: 'url(' + banner.image_url + ')' }"></div>
                             </div>
                             <div v-else>
-                                <a :href="banner.url" aria-label="banner.name">
+                                <a :href="banner.url" :aria-label="banner.name" >
                                     <div class="banner_image" v-bind:style="{ backgroundImage: 'url(' + banner.image_url + ')' }"></div>
                                 </a>
                             </div>
